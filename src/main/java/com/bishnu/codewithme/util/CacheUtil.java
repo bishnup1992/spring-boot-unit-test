8eb11841ae4733e08f10fafc901131750b825b8c
Bishnu <bishnuprasad.mishra1992@gmail.com>
2023-09-08 11:22:40 +0530

    "Java8 Format pushed"

--- src/main/java/com/bishnu/codewithme/util/CacheUtil.java
+++ src/main/java/com/bishnu/codewithme/util/CacheUtil.java
@@ -61,4 +61,51 @@ public class CacheUtil {
     }
 
 
+    // Java 8 Format
+
+    public void refreshPromIdValues(List<CmaReqstDtl> cmaReqDtlsList) {
+        List<PromoInfo> promoInfoList = new ArrayList<>();
+        Map<String, PromoInfo> promoInfoCache = new HashMap<>();
+
+        cmaReqDtlsList.forEach(cmaReqstDtl -> {
+            IntStream.range(1, 10) // Generate a stream of integers from 1 to 9
+                    .mapToObj(i -> "promoId" + i) // Map the integer to promoId attribute name
+                    .map(promoIdAttr -> {
+                        try {
+                            return (String) BeanUtils.getProperty(cmaReqstDtl, promoIdAttr);
+                        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                            // Handle the exception, e.g., log it or take appropriate action
+                            e.printStackTrace();
+                            return null; // Return null in case of an exception
+                        }
+                    })
+                    .filter(promoId -> promoId != null && !promoId.isEmpty()) // Filter non-null and non-empty promoIds
+                    .anyMatch(promoId -> {
+                        PromoInfo cachedPromoInfo = promoInfoCache.get(promoId);
+                        if (cachedPromoInfo != null) {
+                            // Check if promoInfoList already contains cachedPromoInfo
+                            if (!promoInfoList.contains(cachedPromoInfo)) {
+                                promoInfoList.add(cachedPromoInfo);
+                            }
+                            return true; // Exit the loop when promoId is found
+                        } else {
+                            List<PromoInfo> selectedPromoInfo = yourMapper.selectPromoInfo(promoId);
+                            if (!selectedPromoInfo.isEmpty()) {
+                                promoInfoList.addAll(selectedPromoInfo);
+                                selectedPromoInfo.forEach(info -> promoInfoCache.put(info.getPromoId(), info));
+                                return true; // Exit the loop when promoId is found
+                            }
+                        }
+                        return false; // Continue to the next promoId if not found
+                    });
+        });
+
+        // Check if promoInfoList is not empty before batch inserting
+        if (!promoInfoList.isEmpty()) {
+            yourMapper.batchInsertIntoPromoIdDtlsStg(promoInfoList);
+        }
+    }
+
+
+
 }

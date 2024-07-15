8ac3939b47f0685cc398f6938e4f130af7711d6e
Bishnu <bishnuprasad.mishra1992@gmail.com>
2024-07-15 16:49:37 +0530

    "FileUtil"

--- src/main/java/com/bishnu/codewithme/util/FileUtil.java
+++ src/main/java/com/bishnu/codewithme/util/FileUtil.java
@@ -5,10 +5,6 @@ package com.bishnu.codewithme.util;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 package com.example.fragmentservice.util;
 
 import org.springframework.web.multipart.MultipartFile;
@@ -20,17 +16,13 @@ import java.nio.file.Paths;
 
 public class FileUtil {
 
-    public static void storeFile(MultipartFile file, String type, String uploadDir) {
+    public static void storeFile(MultipartFile file, String uploadDir) {
         try {
             if (file.isEmpty()) {
                 throw new RuntimeException("Failed to store empty file.");
             }
 
-            if (!file.getContentType().startsWith("image/")) {
-                throw new RuntimeException("Only image files are allowed.");
-            }
-
-            String fileName = type + "_" + file.getOriginalFilename();
+            String fileName = file.getOriginalFilename();
             Path uploadPath = Paths.get(uploadDir, fileName);
             Files.createDirectories(uploadPath.getParent());
             Files.write(uploadPath, file.getBytes());
@@ -39,3 +31,4 @@ public class FileUtil {
         }
     }
 }
+
